978d4e9104f32232ddc2e45432e8a6bff32afe14
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-06-18 06:23:40 +0300

    add getters and setters

--- src/com/test/domain/CD.java
+++ src/com/test/domain/CD.java
@@ -16,4 +16,52 @@ public class CD {
         this.style = style;
         this.image = image;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Boolean getHot() {
+        return hot;
+    }
+
+    public void setHot(Boolean hot) {
+        this.hot = hot;
+    }
 }
